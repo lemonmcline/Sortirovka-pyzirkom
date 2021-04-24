1176d888b70f251aa4746472998dd04dc6177894
Artyom Pishalnikov <82143842+lemonmcline@users.noreply.github.com>
2021-04-24 12:53:11 +0600

    Sortirovka-pyzirkom
    Sortirovka-pyzirkom

---
+++ Sortirovka pyzirkom.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int z[] = { 1,7,3,6 };
+
+	for (int x = 0; x < 4; x++) {
+		cout << z[x] << " ";
+	}
+	for (int x = 0; x < 4; x++) {
+
+		for (int c = x; c < 4 - 1; c++) {
+			if (z[c] > z[c + 1]); {
+				swap(z[c], z[c + 1]);				
+			}
+		}
+	}
+	cout << endl;
+	for (int x = 0; x < 4; x++) {
+		cout << z[x] << " ";
+	}
+}
